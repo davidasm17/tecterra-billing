78c50f82eece570ef090e3b23c357f178a838e0d
David Alejandro <magoebon17@gmail.com>
2026-01-29 00:02:57 -0600

    Estructura hexagonal inicial

---
+++ src/main/java/com/tecterra/billing/TecterraBillingApplication.java
@@ -0,0 +1,13 @@
+package com.tecterra.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecterraBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecterraBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecterra/billing/TecterraBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecterra.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecterraBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
